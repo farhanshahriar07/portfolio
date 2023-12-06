6296dc0f1919f7552f5c02450f14cfbaf6c03191
Farhan Shahriar <71900033+frhnshhrr@users.noreply.github.com>
2023-12-06 13:59:51 +0600

    Update email.js

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,61 +1,24 @@
-// app.js
+async function sendEmail() {
+    const subject = document.getElementById('subject').value;
+    const text = document.getElementById('text').value;
+    const senderEmail = document.getElementById('senderEmail').value;
+    const senderName = document.getElementById('senderName').value;
 
-const express = require('express');
-const nodemailer = require('nodemailer');
-const bodyParser = require('body-parser');
-const cors = require('cors'); // Import the cors middleware
+    const data = { subject, text, senderEmail, senderName };
 
-const app = express();
-const port = process.env.PORT || 3000;
+    try {
+      const response = await fetch('https://express-mail-api.vercel.app/send-email', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
 
-// Middleware to enable CORS
-app.use(cors());
-
-// Middleware to parse JSON in the request body
-app.use(bodyParser.json());
-
-// POST endpoint for sending emails
-app.post('/send-email', async (req, res) => {
-  try {
-    const { subject, text, senderEmail, senderName } = req.body;
-
-    // Create a nodemailer transporter with your email service credentials
-    const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: 'samanthahaque2@gmail.com',
-        pass: 'pksf adnc udfb hkpq'
-      }
-    });
-
-    // Define the email options
-    const mailOptions = {
-      from:'samanthahaque2@gmail.com', // Use senderEmail if provided, otherwise use default sender
-      to:'frhnshhrr@gmail.com',
-      subject,
-      text: `${senderName ? `From: ${senderName} <${senderEmail}>` : ''}\n\n${text}`, // Add sender info to the text
-    };
-
-    // Send the email
-    const info = await transporter.sendMail(mailOptions);
-
-    console.log('Email sent:', info.response);
-    res.status(200).json({ message: 'Email sent successfully!' });
-  } catch (error) {
-    console.error('Error sending email:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+      const result = await response.json();
+      alert(result.message); // Display the result message
+    } catch (error) {
+      console.error('Error sending email:', error);
+      alert('Error sending email. Please try again.');
+    }
   }
-});
-
-app.get('/', (req, res) => {
-  const serverInfo = {
-    host: req.headers.host,
-    port,
-  };
-  res.json(serverInfo);
-});
-
-// Start the Express server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
